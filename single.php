d53bbfe6d11437eedd674b35e4c0e295e644646a
Ralph Samaniego <rapsamaniego27@gmail.com>
2020-06-27 19:27:11 +0800

    added search, modified archive and single.php

--- single.php
+++ single.php
@@ -1,17 +1,18 @@
 <?php get_header(); ?>
 
-
-
+<div class="container">
       <img src="<?php the_post_thumbnail_url('post_image') ?>">
 
       <h1><?php the_title(); ?></h1>
 
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <?php the_content(); ?>
-      <?php endwhile; else: endif; ?>
+                  <?php the_content(); ?>
+      <?php endwhile;
+      else : endif; ?>
 
       <?php the_tags(); ?>
 
+</div>
+
 
 <?php get_footer(); ?>
-
