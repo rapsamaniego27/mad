7edc6acca690ea70e9ffa301d516da93e1ee2736
Ralph Samaniego <rapsamaniego27@gmail.com>
2020-06-27 12:52:49 +0800

    added templates folder

---
+++ templates/template-home.php
@@ -0,0 +1,17 @@
+<?php 
+/* Template Name: Home */
+get_header(); ?>
+
+<div id="hero">
+  <div class="container d-flex align-items-center justify-content-center h-100">
+    <h1>Welcome to my shop</h1>
+  </div>
+</div>
+
+<div class="content">
+  <div class="container">
+    <h2>Hello</h2>
+  </div>
+</div>
+
+<?php get_footer(); ?>
