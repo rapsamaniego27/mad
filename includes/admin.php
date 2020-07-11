7a42cf43a7caaefc03bc924b85ed83ad7db4eefd
Ralph Samaniego <rapsamaniego27@gmail.com>
2020-07-11 11:10:35 +0800

    added admin code

--- includes/admin.php
+++ includes/admin.php
@@ -1,3 +1,40 @@
 <!-- Admin related functions -->
 
 <?php 
+if( function_exists('acf_add_options_page') ) {
+ 
+ /* Change the name of the options here */
+	acf_add_options_page(array(
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'Website Name Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+ 
+ /* Options to upload logo here */
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Header Settings',
+		'menu_title'	=> 'Header',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Footer Settings',
+		'menu_title'	=> 'Footer',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'General',
+		'menu_title'	=> 'General',
+		'parent_slug'	=> 'theme-general-settings',
+	));	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Social Media',
+		'menu_title'	=> 'Social Media',
+		'parent_slug'	=> 'theme-general-settings',
+	));	
+
+}
+?>
